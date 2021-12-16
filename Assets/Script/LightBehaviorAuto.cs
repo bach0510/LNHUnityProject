7f1716db0caad0232f4eb351835016f1073f93e5
Bachbc0510 <bachdz0510@gmail.com>
2021-12-16 17:43:43 +0700

    change gun light effecet

--- Assets/Script/LightBehaviorAuto.cs
+++ Assets/Script/LightBehaviorAuto.cs
@@ -2,29 +2,17 @@
 using System.Collections;
 
 public class LightBehaviorAuto : MonoBehaviour {
-	public AnimationCurve LightCurve;
-	public float GraphScaleX, GraphScaleY;
-	private float startTime;
 	private Light lightSource;
 
 	void Start(){
 		lightSource = GetComponent<Light>();
 		lightSource.intensity = 0f;
 	}
-
-	void OnEnable () {
-		startTime = Time.time;
-
-	}
 	
 	void Update () {
 		if(Input.GetButtonDown("Fire1") && Input.GetMouseButton(1))
         {
-			var time = Time.time - startTime;
-			if (time > GraphScaleX)
-				startTime = Time.time;
-			var eval = LightCurve.Evaluate( GraphScaleX) / GraphScaleY;
-			lightSource.intensity = 1f;
+			lightSource.intensity = 2f;
 		}
         else
         {
